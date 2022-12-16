26c32eba6a8c10d66640d79eeb24db80b8f2bb64
Dikachi-official <nwokontadikachi@gmail.com>
2022-12-16 18:23:30 +0100

    deployment fix

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast = bool)
 
 ALLOWED_HOSTS = ['web-production-117e.up.railway.app','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-117e.up.railway.app']
+#CSRF_TRUSTED_ORIGINS = ['https://web-production-117e.up.railway.app']
 
 
 # Application definition
